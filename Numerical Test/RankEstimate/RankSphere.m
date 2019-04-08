facbf1eb954f4b078137b5bb52bbd9fb85b13242
JinxinXiong <49404965+JinxinXiong@users.noreply.github.com>
2019-04-09 00:41:26 +0800

    Add files via upload

---
+++ Numerical Test/RankEstimate/RankSphere.m
@@ -0,0 +1,31 @@
+clear
+%由于只需要看大致效果，将算法中的最大迭代次数改为500
+load 1KSphereUniformSampled
+error30 = zeros(1, 5);
+error50 = zeros(1, 5);
+error70 = zeros(1, 5);
+run GenerateDistance30Sphere
+for r = 1: 5
+    D3 = OPTSPACE(Dist.^2, r);
+    D3 = sqrt(D3);
+    D3 = D3 - diag(diag(D3));
+    error30(r) = norm(D3 - Dist_Truth, 'fro')/norm(Dist_Truth, 'fro');
+end
+
+run GenerateDistance50Sphere
+for r = 1: 5
+    D3 = OPTSPACE(Dist.^2, r);
+    D3 = sqrt(D3);
+    D3 = D3 - diag(diag(D3));
+    error50(r) = norm(D3 - Dist_Truth, 'fro')/norm(Dist_Truth, 'fro');
+end
+
+run GenerateDistance70Sphere
+for r = 1: 5
+    D3 = OPTSPACE(Dist.^2, r);
+    D3 = sqrt(D3);
+    D3 = D3 - diag(diag(D3));
+    error70(r) = norm(D3 - Dist_Truth, 'fro')/norm(Dist_Truth, 'fro');
+end
+
+save RankSphere error30 error50 error70
---
+++ Numerical Test/RankEstimate/Test.m
@@ -0,0 +1,2 @@
+run RankSphere
+run RankCow
